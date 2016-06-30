811cb66add82f41b1555ecb0d98a64527a7f8588
huangmingchao <huangmingchao@ahdms.com>
2016-06-30 13:44:15 +0800

    增加头部指令

---
+++ Blog/js/controller/Controller.js
@@ -0,0 +1,7 @@
+/**
+ * Created by huangmingchao on 2016/6/30.
+ */
+var myApp=angular.module("app",[]);
+myApp.controller("indexController",function(){
+
+})
---
+++ Blog/js/directive/directive.js
@@ -0,0 +1,9 @@
+/**
+ * Created by huangmingchao on 2016/6/30.
+ */
+ angular.module('app').directive("hmcHeader",function(){
+     return {
+         restrict:'E',
+         templateUrl:'../../view/header.html',
+     }
+ })
